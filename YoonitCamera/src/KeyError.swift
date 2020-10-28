ec29a75ee1465a38f7a9fb7fb2e9f7fffb9e54bc
HaroldoTeruya <haroldo.s.teruya@gmail.com>
2020-10-28 14:55:48 -0300

    Add KeyError dictionary

---
+++ YoonitCamera/src/KeyError.swift
@@ -0,0 +1,18 @@
+//
+// +-+-+-+-+-+-+
+// |y|o|o|n|i|t|
+// +-+-+-+-+-+-+
+//
+// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+// | Yoonit Camera lib for iOS applications                          |
+// | Haroldo Teruya & Marcio Brufatto @ Cyberlabs AI 2020            |
+// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+//
+
+
+import Foundation
+
+public enum KeyError: String {
+    case NOT_STARTED_PREVIEW = "NOT_STARTED_PREVIEW"
+    case INVALID_CAPTURE_TYPE = "INVALID_CAPTURE_TYPE"
+}
